5b435b343d00b16be63ed6e3cd9a916677c9d62c
fengpei0114 <feng_pei0114@163.com>
2020-11-27 21:56:52 +0800

    add some folder and file, add header in page

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { Header } from '../Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <Header />
       </div>
     );
   }
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.scss';
+
+export const Header = () => {
+  return (
+    <header>
+      <span>分组列表</span>
+      <button type="button">分组学员</button>
+    </header>
+  );
+};
